83886067aec613ae25c1abe594401fd9acc39727
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-13 12:47:53 +0600

    add to display number from keypad

--- app.js
+++ app.js
@@ -14,3 +14,19 @@ function generatePin(){
     const getPin = randomNumber();
     const displayPin = document.getElementById('display-pin').value = getPin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const nInput = document.getElementById('diplay-number');
+    if(isNaN(number)){
+        if(number == 'C'){
+            nInput.value = '';
+        }
+    }
+    else{
+        const prvInput = nInput.value;
+        const prsInput = prvInput + number;
+        nInput.value = prsInput;
+    }
+
+})
